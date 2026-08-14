8c39a82a77e5e5ab3d4aa6556850eeafc9687551
monamohgh <mny990127@gmail.com>
2026-08-15 02:11:59 +0300

    utils files

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class  AppColors {
+  static Color transparentColor = Colors.transparent;
+  static Color primaryColor=Color(0xffF6BD00);
+  static Color blackColor=Color(0xff121312);
+  static Color redColor=Color(0xffE82626);
+  static Color darkGreyColor=Color(0xff282A28);
+  static Color lightGreyColor=Color(0xffADADAD);
+  static Color whiteColor=Colors.white;
+  static Color darkBlackColor=Colors.black;
+}
---
+++ lib/utils/app_styles.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:movies_app/utils/app_colors.dart';
+
+class AppStyles {
+  static TextStyle medium36White = GoogleFonts.inter(
+    fontSize: 36,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.w500,
+  );
+  static TextStyle semi20Black = GoogleFonts.inter(
+    fontSize: 20,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle semi20Primary = GoogleFonts.inter(
+    fontSize: 20,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.w700,
+  );
+  static TextStyle bold24White = GoogleFonts.inter(
+    fontSize: 24,
+    color: AppColors.blackColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle regular20White = GoogleFonts.inter(
+    fontSize: 20,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular16White = GoogleFonts.inter(
+    fontSize: 16,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular15White = GoogleFonts.inter(
+    fontSize: 15,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular15Primary = GoogleFonts.inter(
+    fontSize: 15,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular14Primary = GoogleFonts.inter(
+    fontSize: 14,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular14White = GoogleFonts.inter(
+    fontSize: 14,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular20DarkGrey = GoogleFonts.inter(
+    fontSize: 20,
+    color: AppColors.darkGreyColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular16DarkGrey = GoogleFonts.inter(
+    fontSize: 16,
+    color: AppColors.darkGreyColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle regular16Primary = GoogleFonts.inter(
+    fontSize: 16,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.w400,
+  );
+  static TextStyle bold20LightGrey = GoogleFonts.inter(
+    fontSize: 20,
+    color: AppColors.lightGreyColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold20White = GoogleFonts.inter(
+    fontSize: 20,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold20Primary = GoogleFonts.inter(
+    fontSize: 20,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle bold36White = GoogleFonts.inter(
+    fontSize: 36,
+    color: AppColors.whiteColor,
+    fontWeight: FontWeight.bold,
+  );
+  static TextStyle black14Primary = GoogleFonts.inter(
+    ///black heere means =>w900
+    fontSize: 14,
+    color: AppColors.primaryColor,
+    fontWeight: FontWeight.w900,
+  );
+}
