351620673a6b4629c83915042b251fa3e786838c
monamohgh <mny990127@gmail.com>
2026-08-15 12:34:31 +0300

    update utils files

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/l10n/app_localizations.dart';
+import 'package:movies_app/utils/app_assets.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,8 +9,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
      appBar: AppBar(
-       // title:Text( AppLocalizations.of(context)!.update_data),
+       title:Text( AppLocalizations.of(context)!.update_data),
      ),
+      body: Center(
+        // child: Image.asset(AppAssets.empty),
+      ),
     );
   }
 }
---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,14 @@
+class AppAssets {
+  static const String forgetPassword='assets/images/forgot_password.png';
+  static const String empty='assets/images/empty.png';
+  static const String avatar1='assets/images/avatar1.png';
+  static const String avatar2='assets/images/avatar2.png';
+  static const String avatar3='assets/images/avatar3.png';
+  static const String avatar4='assets/images/avatar4.png';
+  static const String avatar5='assets/images/avatar5.png';
+  static const String avatar6='assets/images/avatar6.png';
+  static const String avatar7='assets/images/avatar7.png';
+  static const String avatar8='assets/images/avatar8.png';
+  static const String avatar9='assets/images/avatar9.png';
+  static const String egypt='assets/images/egypt.png';
+}
