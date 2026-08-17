c7dcde2a808491a2bec29b1199b75c587ded2200
monamohgh <mny990127@gmail.com>
2026-08-17 16:37:36 +0300

    intialize_home_screen

--- lib/ui/screens/home/home_screen.dart
+++ lib/ui/screens/home/home_screen.dart
@@ -1,20 +1,86 @@
 import 'package:flutter/material.dart';
-import 'package:movies_app/l10n/app_localizations.dart';
-import 'package:movies_app/utils/app_assets.dart';
+import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../../providers/app_language_provider.dart';
+import '../../../utils/app_assets.dart';
+import '../../../utils/app_colors.dart';
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int selectedIndex = 0;
+  // List<Widget> tabList = [HomeTab(), SearchTab(), ExploreTab(), ProfileTab()];
+
+  @override
   Widget build(BuildContext context) {
+    var languageProvider = Provider.of<AppLanguageProvider>(context);
     return Scaffold(
-     appBar: AppBar(
-       title:Text( AppLocalizations.of(context)!.update_data),
-     ),
-      body: Center(
-        // child: Image.asset(AppAssets.empty),
-        child: Text('test edit data in  develop branch'),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: AppColors.darkGreyColor,
+        currentIndex: selectedIndex,
+        onTap: (index) {
+          selectedIndex = index;
+          setState(() {});
+        },
+        items: [
+          _builtBottomNavBarItem(
+            selectedIcon: Image.asset(
+              AppAssets.homeIcon,
+              color: AppColors.primaryColor,
+            ),
+            unSelectedIcon: Image.asset(AppAssets.homeIcon),
+            label: '',
+            isSelected: selectedIndex == 0,
+          ),
+          _builtBottomNavBarItem(
+            selectedIcon: Image.asset(
+              AppAssets.searchIcon,
+              color: AppColors.primaryColor,
+            ),
+            unSelectedIcon: Image.asset(AppAssets.searchIcon),
+            label: '',
+            isSelected: selectedIndex == 1,
+          ),
+          _builtBottomNavBarItem(
+            selectedIcon: Image.asset(
+              AppAssets.exploreIcon,
+              color: AppColors.primaryColor,
+            ),
+            unSelectedIcon: Image.asset(AppAssets.exploreIcon),
+            label: '',
+            isSelected: selectedIndex == 2,
+          ),
+          _builtBottomNavBarItem(
+            unSelectedIcon: Image.asset(AppAssets.profileIcon),
+            selectedIcon: Image.asset(
+              AppAssets.profileIcon,
+              color: AppColors.primaryColor,
+            ),
+            label: '',
+            isSelected: selectedIndex == 3,
+          ),
+
+        ],
       ),
+      // body: tabList[selectedIndex],
+
+    );
+  }
+
+  BottomNavigationBarItem _builtBottomNavBarItem({
+    required Widget selectedIcon,
+    required Widget unSelectedIcon,
+    required String label,
+    required bool isSelected,
+  }) {
+    return BottomNavigationBarItem(
+      icon: isSelected ? selectedIcon : unSelectedIcon,
+      label: label,
     );
   }
 }
